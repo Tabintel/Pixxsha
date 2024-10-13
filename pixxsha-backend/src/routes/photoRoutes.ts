ed44781522d5073e8f57ac085c2e564a37fdd376
TobeTek <katchyemma@gmail.com>
2024-10-13 05:27:18 +0100

    chore: List pending API endpoints

--- pixxsha-backend/src/routes/photoRoutes.ts
+++ pixxsha-backend/src/routes/photoRoutes.ts
@@ -10,5 +10,13 @@ router.post('/upload', uploadPhoto);
 router.get('/', getPhotos);
 router.post('/share', sharePhoto);
 router.delete('/:id', deletePhoto);
+// TODO: Add the following routes
+// router.get('/:id', getPhoto);
+// Create a group
+// Delete a group
+// Update the details/info a group
+// List all groups for a user
+// Add a file to a group
+// Remove a file from a group
 
 export const photoRoutes = router;
